7a33ff78cfd96fa2f855f903607e337d39b50a39
Firuz Azizbekov <firuz.azizbekov_2026@ucentralasia.org>
2024-01-14 21:19:16 +0600

    Views are rewritten with generics

--- patient/urls.py
+++ patient/urls.py
@@ -1,29 +1,18 @@
 from django.urls import path
 from .views import (
-    create_emergency_contact,
-    edit_emergency_contact,
-    delete_emergency_contact,
-    get_emergency_contact,
-    create_patient,
-    edit_patient,
-    delete_patient,
-    get_patient,
-    get_all_patients,
+    EmergencyContactList,
+    EmergencyContactDetail,
+    PatientList,
+    PatientDetail
 )
 
 app_name = 'patient'
 
 urlpatterns = [
     # Emergency Contact URLs
-    path('create-emergency-contact/', create_emergency_contact, name='create-emergency-contact'),
-    path('edit-emergency-contact/<int:contact_id>/', edit_emergency_contact, name='edit-emergency-contact'),
-    path('delete-emergency-contact/<int:contact_id>/', delete_emergency_contact, name='delete-emergency-contact'),
-    path('get-emergency-contact/<int:contact_id>/', get_emergency_contact, name='get-emergency-contact'),
-
+    path('emergency-contacts/', EmergencyContactList.as_view(), name='emergency-contact-list'),
+    path('emergency-contacts/<int:pk>/', EmergencyContactDetail.as_view(), name='emergency-contact-detail'),
     # Patient URLs
-    path('create-patient/', create_patient, name='create-patient'),
-    path('edit-patient/<int:patient_id>/', edit_patient, name='edit-patient'),
-    path('delete-patient/<int:patient_id>/', delete_patient, name='delete-patient'),
-    path('get-patient/<int:patient_id>/', get_patient, name='get-patient'),
-    path('get-all-patients/', get_all_patients, name='get-all-patients'),
+    path('patients/', PatientList.as_view(), name='patient-list'),
+    path('patients/<int:pk>/', PatientDetail.as_view(), name='patient-detail'),
 ]
--- patient/views.py
+++ patient/views.py
@@ -1,101 +1,22 @@
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_http_methods
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
 from .models import EmergencyContact, Patient
 from .serializers import EmergencyContactSerializer, PatientSerializer
-from user_authentication.serializers import AddressSerializer
-from user_authentication.models import Address
-
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # ------------- Emergency Contact -------------
+class EmergencyContactList(ListCreateAPIView):
+    queryset = EmergencyContact.objects.all()
+    serializer_class = EmergencyContactSerializer
 
-@csrf_exempt
-@require_http_methods(["POST"])
-def create_emergency_contact(request):
-    serializer = EmergencyContactSerializer(data=request.POST)
-    if serializer.is_valid():
-        emergency_contact = serializer.save()
-        return JsonResponse({"status": "success", "contact_id": emergency_contact.pk})
-    return JsonResponse({"status": "error", "errors": serializer.errors}, status=400)
-
-
-@csrf_exempt
-@require_http_methods(["PUT"])
-def edit_emergency_contact(request, contact_id):
-    emergency_contact = get_object_or_404(EmergencyContact, pk=contact_id)
-    serializer = EmergencyContactSerializer(emergency_contact, data=request.POST, partial=True)
-    if serializer.is_valid():
-        emergency_contact = serializer.save()
-        return JsonResponse({"status": "success", "contact_id": emergency_contact.pk})
-    return JsonResponse({"status": "error", "errors": serializer.errors}, status=400)
-
-
-@csrf_exempt
-@require_http_methods(["DELETE"])
-def delete_emergency_contact(request, contact_id):
-    emergency_contact = get_object_or_404(EmergencyContact, pk=contact_id)
-    emergency_contact.delete()
-    return JsonResponse({"status": "success"})
-
-
-@csrf_exempt
-@require_http_methods(["GET"])
-def get_emergency_contact(request, contact_id):
-    emergency_contact = get_object_or_404(EmergencyContact, pk=contact_id)
-    serializer = EmergencyContactSerializer(emergency_contact)
-    return JsonResponse(serializer.data)
-
+class EmergencyContactDetail(RetrieveUpdateDestroyAPIView):
+    queryset = EmergencyContact.objects.all()
+    serializer_class = EmergencyContactSerializer
 
 # ------------- Patient -------------
 
-@csrf_exempt
-@require_http_methods(["POST"])
-def create_patient(request):
-    if request.method == 'POST':
-        serializer = PatientSerializer(data=request.POST)
-        if serializer.is_valid():
-            patient = serializer.save()
-            return JsonResponse({"status": "success", "patient_id": patient.pk})
-        else:
-            return JsonResponse({"status": "error", "errors": serializer.errors}, status=400)
-    else:
-        return JsonResponse({"status": "error", "message": "GET request not supported for this endpoint"}, status=405)
+class PatientList(ListCreateAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
 
-
-@csrf_exempt
-@require_http_methods(["PUT"])
-def edit_patient(request, patient_id):
-    patient = get_object_or_404(Patient, pk=patient_id)
-    serializer = PatientSerializer(patient, data=request.POST, partial=True)
-    if serializer.is_valid():
-        patient = serializer.save()
-        return JsonResponse({"status": "success", "patient_id": patient.pk})
-    return JsonResponse({"status": "error", "errors": serializer.errors}, status=400)
-
-
-@csrf_exempt
-@require_http_methods(["DELETE"])
-def delete_patient(request, patient_id):
-    patient = get_object_or_404(Patient, pk=patient_id)
-    patient.delete()
-    return JsonResponse({"status": "success"})
-
-
-@csrf_exempt
-@require_http_methods(["GET"])
-def get_patient(request, patient_id):
-    patient = get_object_or_404(Patient, pk=patient_id)
-    serializer = PatientSerializer(patient)
-    return JsonResponse(serializer.data, safe=False)
-
-
-@csrf_exempt
-@require_http_methods(["GET"])
-def get_all_patients(request):
-    patients = Patient.objects.all()
-    serializer = PatientSerializer(patients, many=True)
-    return JsonResponse(serializer.data, safe=False)
+class PatientDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
