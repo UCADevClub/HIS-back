8a5ba7b3295663c2d7267c89186a1e3a3d5bc03e
Firuz Azizbekov <firuz.azizbekov_2026@ucentralasia.org>
2024-01-17 16:29:32 +0600

    Added .vscode to .gitignore and changed the logic of PatientSerializer

--- patient/serializers.py
+++ patient/serializers.py
@@ -10,12 +10,12 @@ class EmergencyContactSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         if validated_data.get('address'):
-            validated_data['address'] = get_or_create(validated_data.pop('address'), Address)
-        return super(PatientSerializer, self).create(validated_data)
+            validated_data['address'] = get_or_create(validated_data.pop('address'), AddressSerializer)
+        return super(EmergencyContactSerializer, self).create(validated_data)
 
     def update(self, instance, validated_data):
         if validated_data.get('address'):
-            validated_data['address'] = get_or_create(validated_data.pop('address'), Address)
+            validated_data['address'] = get_or_create(validated_data.pop('address'), AddressSerializer)
         return super().update(instance, validated_data)
 
     class Meta:
@@ -36,13 +36,13 @@ class PatientSerializer(BaseUserSerializer):
 
     def create(self, validated_data):
         if validated_data.get('primary_address'):
-            validated_data['primary_address'] = get_or_create(validated_data.pop('primary_address'), Address)
+            validated_data['primary_address'] = get_or_create(validated_data.pop('primary_address'), AddressSerializer)
         if validated_data.get('primary_emergency_contact'):
             validated_data['primary_emergency_contact'] = get_or_create(validated_data.pop('primary_emergency_contact'),
-                                                                    EmergencyContact)
+                                                                    EmergencyContactSerializer)
         if validated_data.get('secondary_emergency_contact'):
             validated_data['secondary_emergency_contact'] = get_or_create(validated_data.pop('secondary_emergency_contact'),
-                                                                      EmergencyContact)
+                                                                      EmergencyContactSerializer)
         user = super(PatientSerializer, self).create(validated_data)
         user.set_password(validated_data['password'])
         user.save()
@@ -50,13 +50,13 @@ class PatientSerializer(BaseUserSerializer):
 
     def update(self, instance, validated_data):
         if validated_data.get('primary_address'):
-            validated_data['primary_address'] = get_or_create(validated_data.pop('primary_address'), Address)
+            validated_data['primary_address'] = get_or_create(validated_data.pop('primary_address'), AddressSerializer)
         if validated_data.get('primary_emergency_contact'):
             validated_data['primary_emergency_contact'] = get_or_create(validated_data.pop('primary_emergency_contact'),
-                                                                        EmergencyContact)
+                                                                        EmergencyContactSerializer)
         if validated_data.get('secondary_emergency_contact'):
             validated_data['secondary_emergency_contact'] = get_or_create(
-                validated_data.pop('secondary_emergency_contact'), EmergencyContact)
+                validated_data.pop('secondary_emergency_contact'), EmergencyContactSerializer)
 
         return super().update(instance, validated_data)
 
@@ -76,20 +76,21 @@ class PatientSerializer(BaseUserSerializer):
             'secondary_emergency_contact', 
         ]
         extra_kwargs = {
-            'password': {'write_only': True},
+            'password': {
+                'write_only': True, 
+                'required': False},
         }
 
 
-def get_or_create(data, model):
+def get_or_create(data, serializer):
     # This function is needed because of the nested serializers
-    if type(data) == "<class 'str'>" or type(data) == "<class 'int'>":
-        return get_object_or_404(model, id=data)
+    if isinstance(data, (int, str)):
+        return get_object_or_404(serializer.Meta.model, id=data)
     elif data.get('id'):
-        return get_object_or_404(model, id=data.get('id'))
-    elif model == Address:
-        return Address.objects.create(**data)
-    elif model == EmergencyContact:
-        return EmergencyContact.objects.create(
-            address=get_or_create(data.pop('address'), Address),
-            **data
-        )
+        return get_object_or_404(serializer.Meta.model, id=data.get('id'))
+    s = serializer(data=data)
+    if s.is_valid():
+        return s.save()
+    else:
+        print(s.errors)
+        return s.errors
