1a59e518d795d67862e31f45969075a7de8d620b
Noviero <78156339+nov1ero@users.noreply.github.com>
2024-02-26 17:51:16 +0600

    code clean

--- patient/serializers.py
+++ patient/serializers.py
@@ -2,8 +2,7 @@ from rest_framework.serializers import ModelSerializer, Serializer
 from user_authentication.serializers import (
     BaseUserCreateSerializer,
     BaseUserSerializer,
-    AddressSerializer,
-    AddressCreateSerializer
+    AddressSerializer
 )
 from patient.models import Patient, EmergencyContact
 from user_authentication.models import Address
@@ -46,15 +45,19 @@ class EmergencyContactSerializer(ModelSerializer):
 
     def update(self, instance, validated_data):
 
-        instance.first_name = validated_data.get("first_name", instance.first_name)
-        instance.last_name = validated_data.get("last_name", instance.last_name)
-        instance.phone_number = validated_data.get("phone_number", instance.phone_number)
+        instance.first_name = validated_data.get(
+            "first_name", instance.first_name)
+        instance.last_name = validated_data.get(
+            "last_name", instance.last_name)
+        instance.phone_number = validated_data.get(
+            "phone_number", instance.phone_number)
 
         address_data = validated_data.pop('address', None)
         address_instance = instance.address
 
         if address_data:
-            address_serializer = AddressSerializer(address_instance, data=address_data)
+            address_serializer = AddressSerializer(
+                address_instance, data=address_data)
             if address_serializer.is_valid():
                 address_serializer.save()
         instance.save()
@@ -64,8 +67,10 @@ class EmergencyContactSerializer(ModelSerializer):
         address_data = validated_data.pop('address', None)
         print(f'{validated_data=}')
         print(f'{address_data=}')
-        emergency_contact_instance = EmergencyContact(validated_data=validated_data)
-        emergency_contact_instance.address = Address(validated_data=address_data)
+        emergency_contact_instance = EmergencyContact(
+            validated_data=validated_data)
+        emergency_contact_instance.address = Address(
+            validated_data=address_data)
         return emergency_contact_instance
 
 
@@ -82,8 +87,10 @@ class PatientCreateSerializer(BaseUserCreateSerializer):
         )
 
     def create(self, validated_data):
-        primary_emergency_contact_data = validated_data.pop('primary_emergency_contact')
-        secondary_emergency_contact_data = validated_data.pop('secondary_emergency_contact')
+        primary_emergency_contact_data = validated_data.pop(
+            'primary_emergency_contact')
+        secondary_emergency_contact_data = validated_data.pop(
+            'secondary_emergency_contact')
 
         primary_emergency_contact_instance = EmergencyContactCreateSerializer.create(
             EmergencyContactCreateSerializer(), validated_data=primary_emergency_contact_data
@@ -120,22 +127,14 @@ class PatientSerializer(BaseUserSerializer):
     secondary_emergency_contact = EmergencyContactSerializer()
 
     def update(self, instance, validated_data):
-<<<<<<< HEAD
-
         super().update(instance, validated_data)
 
-        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
-
-        primary_address_data = validated_data.pop('primary_address', None)
-        primary_address_instance = instance.primary_address
-=======
-        super().update(instance, validated_data)
->>>>>>> cf1a8777eb6e838685a1ef4bda499c4024008297
-
-        primary_emergency_contact_data = validated_data.pop('primary_emergency_contact', None)
+        primary_emergency_contact_data = validated_data.pop(
+            'primary_emergency_contact', None)
         primary_emergency_contact_instance = instance.primary_emergency_contact
 
-        secondary_emergency_contact_data = validated_data.pop('secondary_emergency_contact', None)
+        secondary_emergency_contact_data = validated_data.pop(
+            'secondary_emergency_contact', None)
         secondary_emergency_contact_instance = instance.secondary_emergency_contact
 
         if primary_emergency_contact_data:
--- user_authentication/serializers.py
+++ user_authentication/serializers.py
@@ -43,29 +43,12 @@ class BaseUserCreateSerializer(ModelSerializer):
         address_data = validated_data.pop('address')
         address_instance = Address.objects.create(**address_data)
         password = get_random_string(8)
-        base_user_instance = BaseUser.objects.create(**validated_data, password=password, address=address_instance)
+        base_user_instance = BaseUser.objects.create(
+            **validated_data, password=password, address=address_instance)
         return base_user_instance
 
 
 class BaseUserSerializer(ModelSerializer):
-    """
-    Serializer for the BaseUser model.
-
-    Attributes:
-        Meta:
-            model (class): The BaseUser model to be serialized.
-            fields (list): List of fields to include in the serialized representation.
-
-    Examples:
-        To use this serializer, instantiate it in your views or API endpoints:
-
-        ```python
-        serializer = BaseUserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        ```
-
-    """
     class Meta:
         model = BaseUser
         fields = (
@@ -81,7 +64,8 @@ class BaseUserSerializer(ModelSerializer):
         )
 
     def update(self, instance, validated_data):
-        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.phone_number = validated_data.get(
+            'phone_number', instance.phone_number)
         instance.address = validated_data.get('address', instance.address)
         instance.save()
 
@@ -100,86 +84,23 @@ class MainBaseUserSerializer(ModelSerializer):
             'password'
         )
 
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get(
-            'first_name', instance.first_name)
-        instance.last_name = validated_data.get(
-            'last_name', instance.last_name)
-        instance.first_name = validated_data.get(
-            'first_name', instance.first_name)
-        instance.email = validated_data.get('email', instance.email)
-
-        instance.save()
-
-        return instance
-
 
 class AddressSerializer(ModelSerializer):
-<<<<<<< HEAD
-
-    """
-    Serializer for the Address model.
-
-    Attributes:
-        Meta:
-            model (class): The Address model to be serialized.
-            fields (list): List of fields to include in the serialized representation.
-
-    Methods:
-        create(validated_data): Create a new Address instance with the provided data.
-        update(instance, validated_data): Update the fields of an existing Address instance.
-
-    Examples:
-        To use this serializer, instantiate it in your views or API endpoints:
-
-        ```python
-        serializer = AddressSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        ```
-
-    """
-
-=======
->>>>>>> cf1a8777eb6e838685a1ef4bda499c4024008297
     class Meta:
         model = Address
         fields = '__all__'
 
-    def create(self, validated_data):
-        return Address.objects.create(**validated_data)
-
     def update(self, instance, validated_data):
-<<<<<<< HEAD
-        primary_address_data = validated_data.pop('primary_address', None)
-
-        if primary_address_data is not None:
-            # If primary_address_data is provided, update the existing address or create a new one
-            if instance.primary_address:
-                # Update the existing address
-                address_serializer = AddressSerializer(
-                    instance.primary_address, data=primary_address_data, partial=True
-                )
-                if address_serializer.is_valid():
-                    address_serializer.save()
-            else:
-                # Create a new address
-                new_address_serializer = AddressSerializer(data=primary_address_data)
-                if new_address_serializer.is_valid():
-                    new_address_serializer.save()
-                    instance.primary_address = new_address_serializer.instance
-
-        # Update other fields of the main model instance if needed
-        instance.save()
-        return instance
-=======
         instance.country = validated_data.get("country", instance.country)
         instance.oblast = validated_data.get("oblast", instance.oblast)
-        instance.city_village = validated_data.get("city_village", instance.city_village)
+        instance.city_village = validated_data.get(
+            "city_village", instance.city_village)
         instance.street = validated_data.get("street", instance.street)
         instance.house = validated_data.get("house", instance.house)
-        instance.apartment = validated_data.get("apartment", instance.apartment)
-        instance.postal_code = validated_data.get("postal_code", instance.postal_code)
+        instance.apartment = validated_data.get(
+            "apartment", instance.apartment)
+        instance.postal_code = validated_data.get(
+            "postal_code", instance.postal_code)
         instance.save()
         return instance
 
@@ -187,5 +108,3 @@ class AddressSerializer(ModelSerializer):
         address = Address(validated_data)
         address.save()
         return address
-
->>>>>>> cf1a8777eb6e838685a1ef4bda499c4024008297
