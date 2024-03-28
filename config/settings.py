3a790b7dd6a24399b027bf7886a325ff1f047cd5
root <root@example.kg2>
2024-03-28 12:31:50 +0600

    CORS_ALLOW_ALL_ORIGIN

--- config/settings.py
+++ config/settings.py
@@ -190,8 +190,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Developer settings
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = ["*"]
-
+CORS_ALLOW_ALL_ORIGINS = True
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
