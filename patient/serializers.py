d3dd7e373244ac5eb2647a996b8e6e16a3f99230
oneku16 <eku.ulanov@yandex.com>
2024-05-29 15:40:11 +0600

    done with ragistrations

--- patient/serializers.py
+++ patient/serializers.py
@@ -1,9 +1,49 @@
 from rest_framework.serializers import ModelSerializer
 from patient.models import Patient
+from user_authentication.serializers import (
+    StandardUserSerializer,
+)
+from user_authentication.models import (
+    Address,
+    StandardUser,
+    EmergencyContact,
+)
 
 
-class PatientSerializer(ModelSerializer):
+class PatientSerializer(StandardUserSerializer):
 
     class Meta:
         model = Patient
-        fields = '__all__'
+        fields = (
+                'user_id',
+                'first_name',
+                'last_name',
+                'middle_name',
+                'email',
+                'citizenship',
+                'date_of_birth',
+                'phone_number',
+                'gender',
+                'address',
+                'primary_emergency_contact',
+                'secondary_emergency_contact',
+                'marital_status'
+        )
+
+    def create(self, validated_data):
+        address_data = validated_data.pop('address')
+        primary_emergency_contact_data = validated_data.pop('primary_emergency_contact')
+        secondary_emergency_contact_data = validated_data.pop('secondary_emergency_contact')
+
+        address = Address.objects.create(**address_data)
+        primary_emergency_contact = EmergencyContact.objects.create(**primary_emergency_contact_data)
+        if secondary_emergency_contact_data:
+            secondary_emergency_contact = EmergencyContact.objects.create(**secondary_emergency_contact_data)
+        else:
+            secondary_emergency_contact = None
+
+        validated_data['address'] = address
+        validated_data['primary_emergency_contact'] = primary_emergency_contact
+        validated_data['secondary_emergency_contact'] = secondary_emergency_contact
+
+        return StandardUser.objects.create_patient(**validated_data)
