a87470b786868daf79f559102c4a74e1e190809a
muz2002 <muzaffar.nazarkhudoev_2026@ucentralasia.org>
2024-07-19 04:12:08 +0500

    Patient's nested fields can be updated

--- patient/serializers.py
+++ patient/serializers.py
@@ -1,6 +1,8 @@
 from patient.models import Patient
 from user_authentication.serializers import (
     StandardUserSerializer,
+    AddressSerializer,
+    EmergencyContactSerializer
 )
 from user_authentication.models import (
     Address,
@@ -46,3 +48,36 @@ class PatientSerializer(StandardUserSerializer):
         validated_data['secondary_emergency_contact'] = secondary_emergency_contact
 
         return Patient.objects.create_patient(**validated_data)
+
+    def update(self, instance, validated_data):
+        address_data = validated_data.pop('address', None)
+        primary_emergency_contact_data = validated_data.pop('primary_emergency_contact', None)
+        secondary_emergency_contact_data = validated_data.pop('secondary_emergency_contact', None)
+
+        if address_data:
+            address_serializer = AddressSerializer(instance.address, data=address_data, partial=True)
+            if address_serializer.is_valid(raise_exception=True):
+                address_serializer.save()
+
+        if primary_emergency_contact_data:
+            primary_emergency_contact_serializer = EmergencyContactSerializer(
+                instance.primary_emergency_contact, data=primary_emergency_contact_data, partial=True
+            )
+            if primary_emergency_contact_serializer.is_valid(raise_exception=True):
+                primary_emergency_contact_serializer.save()
+
+        if secondary_emergency_contact_data:
+            if instance.secondary_emergency_contact:
+                secondary_emergency_contact_serializer = EmergencyContactSerializer(
+                    instance.secondary_emergency_contact, data=secondary_emergency_contact_data, partial=True
+                )
+            else:
+                secondary_emergency_contact_serializer = EmergencyContactSerializer(data=secondary_emergency_contact_data)
+            if secondary_emergency_contact_serializer.is_valid(raise_exception=True):
+                instance.secondary_emergency_contact = secondary_emergency_contact_serializer.save()
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        
+        instance.save()
+        return instance
