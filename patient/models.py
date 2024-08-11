c57dd303e1a3630fd792244058ea4c26be0a6b36
Noviero <78156339+nov1ero@users.noreply.github.com>
2024-08-12 04:56:29 +0600

    Add: Patient
    Добавил к пациенту
    	    - Blood Group (Группа крови)
    	    - Vision (Зрение)
    	    - Allergies (Аллергии)
    	    - Vaccines (Вакцины)

--- patient/models.py
+++ patient/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from hospital.models import Allergy, Vaccine
 from user_authentication.models import StandardUser
 
-
 class Patient(StandardUser):
+    blood_group = models.CharField(max_length=3, default='UNK')
+    vision = models.CharField(max_length=50, default='UNK')
+    allergies = models.ManyToManyField(Allergy, related_name='patients')
+    vaccines = models.ManyToManyField(Vaccine, related_name='patients')
+    
     is_patient = True
 
     class Meta:
         db_table = 'Patient'
+        
--- patient/serializers.py
+++ patient/serializers.py
@@ -1,59 +1,73 @@
+from rest_framework import serializers
 from patient.models import Patient
 from user_authentication.serializers import (
     StandardUserSerializer,
     AddressSerializer,
     EmergencyContactSerializer
 )
-from user_authentication.models import (
-    Address,
-    EmergencyContact,
-)
+from user_authentication.models import Address, EmergencyContact
+from hospital.models import Allergy, Vaccine
 
 
 class PatientSerializer(StandardUserSerializer):
+    address = AddressSerializer()
+    primary_emergency_contact = EmergencyContactSerializer()
+    secondary_emergency_contact = EmergencyContactSerializer(required=False, allow_null=True)
+    allergies = serializers.PrimaryKeyRelatedField(queryset=Allergy.objects.all(), many=True, required=False)
+    vaccines = serializers.PrimaryKeyRelatedField(queryset=Vaccine.objects.all(), many=True, required=False)
 
     class Meta:
         model = Patient
         fields = (
-                'id',
-                'user_id',
-                'first_name',
-                'last_name',
-                'middle_name',
-                'email',
-                'citizenship',
-                'date_of_birth',
-                'phone_number',
-                'gender',
-                'address',
-                'primary_emergency_contact',
-                'secondary_emergency_contact',
-                'marital_status'
+            'id',
+            'user_id',
+            'first_name',
+            'last_name',
+            'middle_name',
+            'email',
+            'citizenship',
+            'date_of_birth',
+            'phone_number',
+            'gender',
+            'address',
+            'primary_emergency_contact',
+            'secondary_emergency_contact',
+            'marital_status',
+            'blood_group',
+            'vision',
+            'allergies',
+            'vaccines'
         )
 
     def create(self, validated_data):
         address_data = validated_data.pop('address')
         primary_emergency_contact_data = validated_data.pop('primary_emergency_contact')
         secondary_emergency_contact_data = validated_data.pop('secondary_emergency_contact', None)
+        allergies_data = validated_data.pop('allergies', [])
+        vaccines_data = validated_data.pop('vaccines', [])
 
         address = Address.objects.create(**address_data)
         primary_emergency_contact = EmergencyContact.objects.create(**primary_emergency_contact_data)
-        if secondary_emergency_contact_data:
-            secondary_emergency_contact = EmergencyContact.objects.create(**secondary_emergency_contact_data)
-        else:
-            secondary_emergency_contact = None
+        secondary_emergency_contact = EmergencyContact.objects.create(**secondary_emergency_contact_data) if secondary_emergency_contact_data else None
 
-        validated_data['address'] = address
-        validated_data['primary_emergency_contact'] = primary_emergency_contact
-        validated_data['secondary_emergency_contact'] = secondary_emergency_contact
+        patient = Patient.objects.create(
+            address=address,
+            primary_emergency_contact=primary_emergency_contact,
+            secondary_emergency_contact=secondary_emergency_contact,
+            **validated_data
+        )
 
-        return Patient.objects.create_patient(**validated_data)
+        patient.allergies.set(allergies_data)
+        patient.vaccines.set(vaccines_data)
+
+        return patient
 
     def update(self, instance, validated_data):
-        
         address_data = validated_data.pop('address', None)
         primary_emergency_contact_data = validated_data.pop('primary_emergency_contact', None)
         secondary_emergency_contact_data = validated_data.pop('secondary_emergency_contact', None)
+        allergies_data = validated_data.pop('allergies', [])
+        vaccines_data = validated_data.pop('vaccines', [])
 
         if address_data:
             address_serializer = AddressSerializer(instance.address, data=address_data, partial=True)
@@ -76,9 +90,12 @@ class PatientSerializer(StandardUserSerializer):
                 secondary_emergency_contact_serializer = EmergencyContactSerializer(data=secondary_emergency_contact_data)
             if secondary_emergency_contact_serializer.is_valid(raise_exception=True):
                 instance.secondary_emergency_contact = secondary_emergency_contact_serializer.save()
-        
+
+        instance.allergies.set(allergies_data)
+        instance.vaccines.set(vaccines_data)
+
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
-        
+
         instance.save()
         return instance
